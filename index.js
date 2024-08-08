e2887f033344558a2769db630a8dc3bc20c67e66
Naoyuki Ishida <39.ishida@gmail.com>
2024-08-08 00:26:54 -0700

    feat: advanced option using yargs

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const path = require('path');
 const fs = require('fs');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+
+const { argv } = yargs(hideBin(process.argv))
+  .option('name', {
+    describe: 'Show the name of the CLI tool'
+  })
+  .option('file', {
+    describe: 'The path of the Markdown file'
+  });
+
 
 const packageStr = fs.readFileSync(path.resolve(__dirname, 'package.json'), { encoding: 'utf-8' });
 const package = JSON.parse(packageStr);
 
-const nameOption = process.argv.includes('--name');
-
-if (nameOption) {
+if (argv.file) {
+  console.log(argv.file);
+} else if (argv.name) {
   console.log(package.name);
 } else {
   console.log('No option.')
